533e318633bf97e0c2ca5dda9bef22ac8d19e36b
umutatalay <umutatalayumut@gmail.com>
2021-02-16 11:38:26 +0300

    ilk

---
+++ Pproje/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using PagedList.Core;
+
+using Pproje.Models;
+
+namespace Pproje.Controllers
+{
+    [Route("Product")]
+    public class ProductController : Controller
+    {
+        private DataContext _db = new DataContext();
+
+        [Route("index")]
+        [Route("")]
+        [Route("~/")]
+        public IActionResult Index(int page = 1, int pageSize = 4)
+        {
+            PagedList<Product> model = new PagedList<Product>(_db.Product, page, pageSize);
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ Pproje/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pproje.Models
+{
+    [Table("Product")]
+
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        
+    }
+}
